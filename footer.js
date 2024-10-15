e61bf7a5e74f07068b662bd88a030514268b66cd
Ultimmm <spsudumalpet@gmail.com>
2024-10-15 18:29:05 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-6">
+      <div className="max-w-7xl mx-auto px-4">
+        <p>© 2024 NewsSite. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
