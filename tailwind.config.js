b2cf4ad49f6c2c08090ccc3521987dacbd8f1a6b
Ultimmm <spsudumalpet@gmail.com>
2024-10-15 18:20:54 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // Enables dark mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
